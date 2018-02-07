37b5df1231956f5842515de6c8b304231f6b82f1
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-07 16:38:44 +0100

    Windrichting en controletower

--- src/main/java/com/capgemini/TrafficControlTower.java
+++ src/main/java/com/capgemini/TrafficControlTower.java
@@ -2,4 +2,20 @@ package com.capgemini;
 
 public class TrafficControlTower {
 
+    private WindDirection windDirection;
+
+    private double windSpeed;
+
+    private boolean permissionToLand;
+
+    //private Plane plane;
+    //private Runway runway;
+
+
+
+    public static boolean runWayFree(){
+        // deze geeft boolean terug als runway free is
+        // Hier moet if statements komen voor de runway
+        return true;
+    }
 }
---
+++ src/main/java/com/capgemini/WindDirection.java
@@ -0,0 +1,5 @@
+package com.capgemini;
+
+public enum WindDirection {
+    NORTH, EAST, SOUTH, WEST
+}
