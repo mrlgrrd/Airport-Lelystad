a833c7d7b7c0aa4a6711c034a3069b318176e28c
mrlgrrd <35731373+mrlgrrd@users.noreply.github.com>
2018-02-07 16:09:34 +0100

    update security

--- src/main/java/com/capgemini/Security.java
+++ src/main/java/com/capgemini/Security.java
@@ -1,4 +1,13 @@
 package com.capgemini;
 
 public class Security {
+
+    private String typePersonnel;
+
+    private Personnel personnel;
+
+    private String secLocation;
+
+    private String secEmployeeAmount;
+
 }
