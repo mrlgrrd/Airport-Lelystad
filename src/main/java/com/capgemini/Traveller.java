7af2b9939f84f899273df13a8080dd4a40c051e6
suuzels <suzannevdmeer@gmail.com>
2018-02-07 16:00:08 +0100

    added some properties

--- src/main/java/com/capgemini/Traveller.java
+++ src/main/java/com/capgemini/Traveller.java
@@ -1,4 +1,21 @@
 package com.capgemini;
 
 public class Traveller {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String destination;
+
+    private int age;
+
+    private boolean isCheckedIn;
+
+    private boolean hasBoardingPass;
+
+    private boolean hasCarryOnLuggage;
+
+    private boolean hasBaggage;
+
 }
