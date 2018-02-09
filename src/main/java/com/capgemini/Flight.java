b9ed0529e57a74c59cc1fe3ae5c803372924161e
suuzels <suzannevdmeer@gmail.com>
2018-02-09 11:00:03 +0100

    .

--- src/main/java/com/capgemini/Flight.java
+++ src/main/java/com/capgemini/Flight.java
@@ -2,6 +2,8 @@ package com.capgemini;
 
 public class Flight {
 
+    // Properties
+
     private String destination;
 
     private String origin;
@@ -12,5 +14,55 @@ public class Flight {
 
     private double departureTime;
 
-    public Vehicle aircraft;
+    public Vehicle aircraft; // change to aircraft identifier
+
+    // Getters and setters
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getFlightID() {
+        return flightID;
+    }
+
+    public void setFlightID(String flightID) {
+        this.flightID = flightID;
+    }
+
+    public double getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(double arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public double getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(double departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public Vehicle getAircraft() {
+        return aircraft;
+    }
+
+    public void setAircraft(Vehicle aircraft) {
+        this.aircraft = aircraft;
+    }
 }
