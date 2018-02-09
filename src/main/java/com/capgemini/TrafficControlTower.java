fa10a9a25952f9f586e3662a351f6f057c9b4a18
mrlgrrd <35731373+mrlgrrd@users.noreply.github.com>
2018-02-09 11:18:23 +0100

    update

--- src/main/java/com/capgemini/TrafficControlTower.java
+++ src/main/java/com/capgemini/TrafficControlTower.java
@@ -8,10 +8,20 @@ public class TrafficControlTower {
 
     private boolean permissionToLand;
 
+    private Plane plane;
+
+    private Runway runway;
 
 
 
+    public boolean askPermissionToLand(){
         // deze geeft boolean terug als runway free is
         // Hier moet if statements komen voor de runway
+        return;
+    }
+
+    public void askPermissionToTakeOff(){
+
+        return;
     }
 }
