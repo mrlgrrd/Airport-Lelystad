18795bdfd6a8673fb7c5b5d6c8970dfefa1c3896
suuzels <suzannevdmeer@gmail.com>
2018-02-09 11:24:29 +0100

    added a class for personal details

--- src/main/java/com/capgemini/FlightCompany.java
+++ src/main/java/com/capgemini/FlightCompany.java
@@ -2,6 +2,12 @@ package com.capgemini;
 
 public class FlightCompany {
 
+    // Constructor
+
+    public FlightCompany(String companyName){
+        this.companyName = companyName;
+    }
+
     // Properties
     private String companyName;
 
@@ -11,7 +17,48 @@ public class FlightCompany {
 
     private double vatNumber;
 
-    
+    private double totalNumberOfPersonnel;
+
+    // Getters and setters
 
 
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public ENationality getEnationality() {
+        return enationality;
+    }
+
+    public void setEnationality(ENationality enationality) {
+        this.enationality = enationality;
+    }
+
+    public int getNumberOfAircraft() {
+        return numberOfAircraft;
+    }
+
+    public void setNumberOfAircraft(int numberOfAircraft) {
+        this.numberOfAircraft = numberOfAircraft;
+    }
+
+    public double getVatNumber() {
+        return vatNumber;
+    }
+
+    public void setVatNumber(double vatNumber) {
+        this.vatNumber = vatNumber;
+    }
+
+    public double getTotalNumberOfPersonnel() {
+        return totalNumberOfPersonnel;
+    }
+
+    public void setTotalNumberOfPersonnel(double totalNumberOfPersonnel) {
+        this.totalNumberOfPersonnel = totalNumberOfPersonnel;
+    }
 }
