5ed4eeed2556e270c4746594f0a5f0d2685c9d78
mrlgrrd <35731373+mrlgrrd@users.noreply.github.com>
2018-02-09 12:12:21 +0100

    update

--- src/main/java/com/capgemini/CheckIn.java
+++ src/main/java/com/capgemini/CheckIn.java
@@ -10,12 +10,21 @@ public class CheckIn {
             System.out.println("You are not allowed to use Self Check In.");
         }
     }
+
     // execute the check in at the desk. check if the check in is allowed
+    public void checkInDesk(boolean checkInDesk) {
+        if (checkInDesk) {
             System.out.println("You have been checked in.");
+        } else {
             System.out.println("You are not allowed to use the check in desk");
+        }
     }
+
     // execute the baggage drop off. check if the baggage drop off is allowed
+    public void dropBaggageOff(boolean baggageDroppedOff) {
+        if (baggageDroppedOff) {
             System.out.println("Thank you for dropping off your baggage");
+        } else {
             System.out.println("You are not allowed to use baggage drop off.");
         }
     }
---
+++ src/main/java/com/capgemini/EFlightDestinationType.java
@@ -0,0 +1,5 @@
+package com.capgemini;
+
+public enum EFlightDestinationType {
+    EUROPE, NORTHAMERICA, SOUTHAMERICA, ASIA, AUSTRALIA, AFRICA
+}
