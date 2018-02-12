a856e5c5b7c9457bc5ec7554bc5444658c61c7d0
suuzels <suzannevdmeer@gmail.com>
2018-02-12 16:16:16 +0100

    klasse adres aangemaakt.

---
+++ src/main/java/com/capgemini/Address.java
@@ -0,0 +1,16 @@
+package com.capgemini;
+
+public class Address {
+
+    private String streetName;
+
+    private int houseNumber;
+
+    private String postalCode;
+
+    private String city;
+
+    private String country;
+
+    
+}
