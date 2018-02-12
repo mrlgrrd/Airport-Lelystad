f6b1676f3fe2f0f1342e5df315e6aae99d6409e3
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-12 16:32:18 +0100

    Airportmanager heeft een toren en runways

--- src/main/java/com/capgemini/AirportManager.java
+++ src/main/java/com/capgemini/AirportManager.java
@@ -1,7 +1,13 @@
 package com.capgemini;
 
 public class AirportManager {
+
     // aanmaken nieuwe controle toren
     TrafficControlTower trafficControlTower = new TrafficControlTower();
+
+
+    // aanmaken van nieuwe runways
+    Runway runway1 = new Runway(EWindDirection.NORTH, false, false);
+    Runway runway2 = new Runway(EWindDirection.SOUTH, false, false);
 
 }
