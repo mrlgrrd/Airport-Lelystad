14ef32aadc1b1d0f8d8bddcc0b8099269a064add
Daniëlle Duijst <dmsduijst@gmail.com>
2018-02-12 16:37:14 +0100

    dubbele airportmanager verwijderd

--- src/main/java/com/capgemini/manager/AirportManager.java
+++ src/main/java/com/capgemini/manager/AirportManager.java
@@ -1,4 +1,15 @@
 package com.capgemini.manager;
 
+import com.capgemini.EWindDirection;
+import com.capgemini.Runway;
+import com.capgemini.TrafficControlTower;
+
 public class AirportManager {
+    // aanmaken nieuwe controle toren
+    TrafficControlTower trafficControlTower = new TrafficControlTower();
+
+
+    // aanmaken van nieuwe runways
+    Runway runway1 = new Runway(EWindDirection.NORTH, false, false);
+    Runway runway2 = new Runway(EWindDirection.SOUTH, false, false);
 }
