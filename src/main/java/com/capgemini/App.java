7f9c4acd0634150d54c74cc60fe24949f9072523
tjitjert <rich.hoving@gmail.com>
2018-02-15 16:20:59 +0100

    HEAD ERROR DELETED

--- src/main/java/com/capgemini/App.java
+++ src/main/java/com/capgemini/App.java
@@ -13,13 +13,12 @@ import com.capgemini.security.Security;
 public class App {
     public static void main(String[] args) {
 
-<<<<<<< HEAD
-=======
+
         System.out.println("Hello Lelystad!");
 
         AirportManager airportManager = new AirportManager();
         airportManager.startAirport();
->>>>>>> aeb2a9040121b00f5cb6c358ed4e348e7c260c78
+
 
 
 
