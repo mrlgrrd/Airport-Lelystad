bced0bb971e170049e2ae27a36a42d0a25a67c99
suuzels <suzannevdmeer@gmail.com>
2018-02-15 15:32:11 +0100

    .

--- src/main/java/com/capgemini/manager/PeopleManager.java
+++ src/main/java/com/capgemini/manager/PeopleManager.java
@@ -1,19 +1,33 @@
 package com.capgemini.manager;
 
+import com.capgemini.CheckIn;
 import com.capgemini.person.traveller.Traveller;
 
 import java.util.ArrayList;
 
 public class PeopleManager {
-    // hier worden nieuwe travellers gegenereerd
+
+    private Traveller traveller;
+
+    private CheckIn checkIn;
+
    public void makeTravellers() {
        ArrayList<Traveller> travellers = new ArrayList<Traveller>();
 
-   }
+       // hier worden nieuwe travellers gegenereerd
+       Traveller robbert = new Traveller();
 
+   }
     // hier worden nieuwe employees gegenereerd
 
+    
+
     // hier wordt het proces van traveller die incheckt aangeroepen
+
+
+
+
+
 
 
 
