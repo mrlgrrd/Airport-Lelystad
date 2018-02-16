4d08823b245b1c88a597d7b3ef9b4425907f5cc4
SashaVollebregt <SashaVollebregt@users.noreply.github.com>
2018-02-16 09:31:25 +0100

    I fixed
    fixed

--- src/main/java/com/capgemini/manager/AirportManager.java
+++ src/main/java/com/capgemini/manager/AirportManager.java
@@ -1,13 +1,9 @@
 package com.capgemini.manager;
 
-<<<<<<< HEAD
-
 import com.capgemini.CheckIn;
 import com.capgemini.EWindDirection;
 import com.capgemini.Runway;
 import com.capgemini.TrafficControlTower;
-
-=======
 import com.capgemini.*;
 import com.capgemini.person.traveller.Traveller;
 import com.capgemini.security.Security;
@@ -22,77 +18,71 @@ public class AirportManager {
 
     Traveller traveller;
     CheckIn checkIn;
->>>>>>> 3eccb92caee92f1b2bbbb0ca0662d507ddf66539
 
-public class AirportManager {
-    // aanmaken nieuwe controle toren
-    TrafficControlTower trafficControlTower = new TrafficControlTower();
+        // aanmaken nieuwe controle toren
+        TrafficControlTower trafficControlTower = new TrafficControlTower();
 
-    // aanmaken van nieuwe runways
-    Runway runway1 = new Runway(EWindDirection.NORTH, false, false);
-    Runway runway2 = new Runway(EWindDirection.SOUTH, false, false);
+        // aanmaken van nieuwe runways
+        Runway runway1 = new Runway(EWindDirection.NORTH, false, false);
+        Runway runway2 = new Runway(EWindDirection.SOUTH, false, false);
 
-    // aanmaken van checkInDesk
-<<<<<<< HEAD
-    CheckIn checkInArea = new CheckIn();
-=======
+        // aanmaken van checkInDesk
 
+        public void makeCheckIn() {
+            CheckIn checkInArea = new CheckIn();
+            this.checkIn = checkInArea;
+            checkInArea.checkInDesk(this.traveller);
+        }
 
-    public void makeCheckIn() {
-        CheckIn checkInArea = new CheckIn();
-        this.checkIn = checkInArea;
-        checkInArea.checkInDesk(this.traveller);
-    }
->>>>>>> 3eccb92caee92f1b2bbbb0ca0662d507ddf66539
+        // creeren van de peoplemanager om de travellers en employees aan te maken
 
+        private void makePeopleManager() {
+            PeopleManager peopleManager = new PeopleManager();
+            System.out.println("people manager made");
+        }
 
-    // creeren van de peoplemanager om de travellers en employees aan te maken
+        /**
+         * this method creates the security
+         */
+        public void makeSecurity() {
+            Security security = new Security();
+            security.checkSecurityClearance(traveller);
+        }
 
-    private void makePeopleManager() {
-        PeopleManager peopleManager = new PeopleManager();
-        System.out.println("people manager made");
-    }
+        // start het vliegveld
+        public void startAirport() {
+            makePeopleManager();
+            makeTravellers();
+            makeCheckIn();
+            makeSecurity();
+            makeCustoms();
+            System.out.println("You may enter the plane.");
+            System.out.println("Enjoy your flight!");
+            System.out.println("                                                                   \n" +
+                    "                            !                                                  \n" +
+                    "                            !                                                  \n" +
+                    "                           /_\\                                                 \n" +
+                    "                    =====/` - '\\=====                                          \n" +
+                    "                        ( ( O ) )                                              \n" +
+                    " --______-------________/\\  -  /\\_______--------______------           \n" +
+                    "      ---------____***___/`---'\\___***____---------- ");
 
-    /**
-     * this method creates the security
-     */
-    public void makeSecurity(){
-        Security security = new Security();
-        security.checkSecurityClearance(traveller);
-    }
+        }
 
-    // start het vliegveld
-    public void startAirport() {
-        makePeopleManager();
-        makeTravellers();
-        makeCheckIn();
-        makeSecurity();
-        makeCustoms();
-        System.out.println("You may enter the plane.");
-        System.out.println("Enjoy your flight!");
-        System.out.println("                                                                   \n" +
-                "                            !                                                  \n" +
-                "                            !                                                  \n" +
-                "                           /_\\                                                 \n" +
-                "                    =====/` - '\\=====                                          \n" +
-                "                        ( ( O ) )                                              \n" +
-                " --______-------________/\\  -  /\\_______--------______------           \n" +
-                "      ---------____***___/`---'\\___***____---------- ");
+        public void makeTravellers() {
+            // hier worden nieuwe travellers gegenereerd
 
-    }
+            Traveller robbert = new Traveller(ECheckInType.SELFCHECKIN, false, true);
+            this.traveller = robbert;
+            System.out.println("robbert made");
 
-    public void makeTravellers() {
-        // hier worden nieuwe travellers gegenereerd
+        }
 
-        Traveller robbert = new Traveller(ECheckInType.SELFCHECKIN, false, true);
-        this.traveller = robbert;
-        System.out.println("robbert made");
+        public void makeCustoms() {
+            Customs customs = new Customs();
+            customs.checkEU(traveller);
+        }
 
-    }
-
-    public void makeCustoms(){
-        Customs customs = new Customs();
-        customs.checkEU(traveller);
     }
 
-}
+
